1740b306d5c57551b0a532cf0e412c0300c55f2c
Husgie Du <husgie_du@limei-trading.com>
2021-09-07 18:18:34 +0800

    测试ETF排序rank

---
+++ app/Console/Commands/RankingETF.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Redis;
+use App\Models\Market\Stock;
+
+class RankingETF extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'rank:etf';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Rank ETF every 5 mins';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        // HKEX:ETF:Ranking:CurPrice
+        // HKEX:ETF:Ranking:ChgRatio
+        // HKEX:ETF:Ranking:Volume
+        // HKEX:ETF:Ranking:Turnover
+        
+        $fields = ['nominal_price', 'chg_ratio', 'total_volume', 'total_turnover'];
+        
+        $rows = Stock::where('type', 'Trust')->whereIn('subType', [10, 16, 17, 18])->get(['stockCode']);
+        
+        foreach ($rows as $row) {
+            $key = "HKEX:Trst:{$row->stockCode}:Info";
+            
+            $info = Redis::hMGet($key, $fields);
+            $info = array_combine($fields, $info);
+            
+            if ($info['nominal_price'] !== false) {
+                Redis::zAdd('HKEX:ETF:Ranking:CurPrice', $info['nominal_price'], $row->stockCode);
+            }
+            if ($info['chg_ratio'] !== false) {
+                Redis::zAdd('HKEX:ETF:Ranking:ChgRatio', $info['chg_ratio'], $row->stockCode);
+            }
+            if ($info['total_volume'] !== false) {
+                Redis::zAdd('HKEX:ETF:Ranking:Volume', $info['total_volume'], $row->stockCode);
+            }
+            if ($info['total_turnover'] !== false) {
+                Redis::zAdd('HKEX:ETF:Ranking:Turnover', $info['total_turnover'], $row->stockCode);
+            }
+        }
+    }
+}
---
+++ app/Console/Commands/RankingETFSlow.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Console\Commands;
+
+bcscale(3);
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Redis;
+use App\Models\Market\Stock;
+use App\Models\Market\Company;
+
+class RankingETFSlow extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'rank:etf_slow';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Rank ETF slowly';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        // HKEX:ETF:Ranking:MarketCap
+        
+        $fields = ['nominal_price'];
+        
+        $rows = Stock::where('type', 'Trust')->whereIn('subType', [10, 16, 17, 18])->get(['stockCode']);
+        
+        foreach ($rows as $row) {
+            $key = "HKEX:Trst:{$row->stockCode}:Info";
+            
+            $info = Redis::hMGet($key, $fields);
+            $info = array_combine($fields, $info);
+            
+            if ($info['nominal_price'] !== false) {
+                $market_cap = '0';
+                
+                $company = Company::where('stockCode', $row->stockCode)->first(['marketData.cap.issue']);
+                if (!is_null($company) && isset($company['marketData']['cap']['issue'])) {
+                    if (strtoupper($company['marketData']['cap']['issue']) !== 'N/A') {
+                        $market_cap = bcmul($info['nominal_price'], $company['marketData']['cap']['issue']);
+                    }
+                }
+                
+                Redis::zAdd('HKEX:ETF:Ranking:MarketCap', $market_cap, $row->stockCode);
+            }
+        }
+    }
+}

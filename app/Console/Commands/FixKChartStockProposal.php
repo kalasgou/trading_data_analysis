24bff5dd46462aa1086c2d32b24fa9ff88317431
Husgie Du <husgie_du@limei-trading.com>
2021-10-11 11:31:56 +0800

    test hkex stock proposal recalculation

--- app/Console/Commands/FixKChartStockProposal.php
+++ app/Console/Commands/FixKChartStockProposal.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Artisan;
 use App\Models\Company\CompanyPly;
 
 class FixKChartStockProposal extends Command
@@ -38,6 +39,50 @@ class FixKChartStockProposal extends Command
      */
     public function handle()
     {
-        //
+        $dimensions = ['day', 'week', 'month', 'quarter', 'year'];
+        $ex_code = 'HKEX';
+        
+        $today_ts = strtotime('2019-06-22'); // mktime(0,0,0);
+        $first_hkex_ts = strtotime('2019-06-21');
+        
+        $rows = CompanyPly::where('exchange_code', '=', $ex_code)->where('list.end_time', '>=', $today_ts)->get();
+        
+        foreach ($rows as $row) {
+            foreach ($row->list as $one) {
+                if ($one['start_time'] > $first_hkex_ts && $one['start_time'] <= time()) {
+                    $date = date('Y-m-d', $one['start_time']);
+                    foreach ($dimensions as $dim) {
+                        Artisan::call('indicator:kchart', [
+                            '--exchange_code' => $ex_code, 
+                            '--stock_code' => $one['stock_code_temporary'], 
+                            '--prdt_type' => 'Equity', 
+                            '--dimension' => $dim, 
+                            '--start_date' => $date, 
+                            '--end_date' => $date
+                        ]);
+                    }
+                    
+                    echo 'tmp: ', $one['stock_code_temporary'], '#';
+                }
+                
+                if ($one['middle_time'] > $first_hkex_ts && $one['middle_time'] <= time()) {
+                    $date = date('Y-m-d', $one['middle_time']);
+                    foreach ($dimensions as $dim) {
+                        Artisan::call('indicator:kchart', [
+                            '--exchange_code' => $ex_code, 
+                            '--stock_code' => $row->stock_code_new, 
+                            '--prdt_type' => 'Equity', 
+                            '--dimension' => $dim, 
+                            '--start_date' => $date, 
+                            '--end_date' => $date
+                        ]);
+                    }
+                    
+                    echo 'new: '$row->stock_code_new;
+                }
+                
+                echo PHP_EOL;
+            }
+        }
     }
 }
